63e3fc26b69124d3abc8211dde3d909d18a14ad5
ST10485100 <ST10485100@vcconnect.edu.za>
2026-03-24 21:03:25 +0200

    Part 1 question 1, welcome voice

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Media;
+using static System.Net.Mime.MediaTypeNames;
+namespace PROG6221_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1 Question 1
+            string audioPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "POE Part1 Q1.wav");
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(audioPath);
+            player.Play();
+            Console.WriteLine("Press Enter to stop the music...");
+            Console.ReadLine();
+        }
+    }
+}
