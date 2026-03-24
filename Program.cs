163c2ea1b43350c34d41b181b299612dfaac8de1
ST10485100 <ST10485100@vcconnect.edu.za>
2026-03-24 21:25:37 +0200

    Part 1 Question 2, ASCII logo

--- Program.cs
+++ Program.cs
@@ -7,12 +7,21 @@ namespace PROG6221_POE
     {
         static void Main(string[] args)
         {
-            //Part 1 Question 1
+
+            //Part 1 Question 2, ASCII logo
+            String msg = "CSAB";
+
+            String asc = FigletFontArt.FiggleFonts.Standard.Render(msg);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(asc);
+            Console.ResetColor();
+
+            //Part 1 Question 1, welcome voice
             string audioPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "POE Part1 Q1.wav");
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(audioPath);
             player.Play();
             Console.WriteLine("Press Enter to stop the music...");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
